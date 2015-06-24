e368365cee2380dd23cb40c820be7d14fa9e07de
mateusferrer <mateusferrer.silva@gmail.com>
2015-06-23 23:12:41 -0300

    ##Estrutura##
    Alterações de Estrutura no Projeto no modelo MVC

---
+++ NovaProject/NovaProject/Dao/DaoGenerico.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NovaProject.Dao
+{
+    public class DaoGenerico
+    {
+        /**
+         * Metodo para inserir um objeto qualquer
+         */
+        public bool inserir(Object obj)
+        {
+
+            return true;
+        }
+
+        /**
+         * Metodo para editar um objeto qualquer
+         */
+        public bool editar(Object obj)
+        {
+
+            return true;
+        }
+
+        /**
+         * Metodo para excluir um objeto qualquer
+         */
+        public bool excluir(Object obj)
+        {
+
+            return true;
+        }
+
+        /**
+         * Metodo utilizado para consultar todos os objetos no bancos
+         */
+        public List<Object> consultarTudo()
+        {
+
+            return null;
+        }
+
+    }
+}
---
+++ NovaProject/NovaProject/Models/Cadastro/Usuario.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NovaProject.Models.Cadastro
+{
+    public class Usuario
+    {
+    }
+}
